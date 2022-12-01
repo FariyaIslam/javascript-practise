cf1e5012689858e2e329c4d304999716c6d616a5
Fariya Islam <fariyaislam2096@gmail.com>
2022-12-01 21:15:36 +0600

    loop practise

---
+++ function_excecise.js
@@ -0,0 +1,12 @@
+// https://contactmentor.com/js-function-exercises-solution/
+// 1. Write a JavaScript function to print the “Hello World” message
+// 2. Write a function that returns the square of a number
+// 3. Write a function to convert Celsius to Fahrenheit
+// 4. Write a function to find the area of a given Rectangle
+// 5. Write a function to find the area and perimeter of a Circle
+// 6. Write a function to reverse a number
+// 7. Count number of Vowels in String
+// 8. Flatten array of arrays using JavaScript reduce
+// 9. Write a function to check if an input string is a palindrome
+// 10. Write a function to calculate simple interest based on the principle amount
+// 11. Write a function to calculate compound interest based on the principle amount
---
+++ functions.js
@@ -0,0 +1,12 @@
+// function sum(num1, num2) {
+//     return parseInt(num1) + parseInt(num2);
+// }
+
+// let res = sum('23', '10');
+// console.log(typeof (res));
+
+const multiply = (num1, num2) => {
+    return num1 * num2;
+}
+let res = multiply(44, 22);
+console.log(res)
